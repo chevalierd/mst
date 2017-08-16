d41f50ee6891393ac16e0af25a2df23d5543fe47
Dominic Chevalier <dcchevalier@gmail.com>
2017-08-15 20:41:09 -0500

    use classes

--- switch/main.cc
+++ switch/main.cc
@@ -1,126 +1,172 @@
 #include "Servo.h"
 #include "Conceptinetics.h"
+#include <vector>
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <algorithm>
 
 using namespace std;
 
 // twelve servo objects can be created on most boards
 const int DMX_SLAVE_CHANNELS = 3;
-Servo servoList[DMX_SLAVE_CHANNELS];
-
-// Configure a DMX slave controller
-DMX_Slave dmx_slave ( DMX_SLAVE_CHANNELS );
 
 enum SwitchPosition {NEUTRAL, ON, OFF};
-SwitchPosition currentPosition[DMX_SLAVE_CHANNELS]={NEUTRAL};
-unsigned long availableToMoveAgain[DMX_SLAVE_CHANNELS]={0};
-unsigned long pendingMoveToNeutral[DMX_SLAVE_CHANNELS]={0};
+
 const int MOVE_DELAY = 2500;  // delay in miliseconds before returning to neutral position
 
-void moveOff(int servo) {
-  if(servo < DMX_SLAVE_CHANNELS) {
-    servoList[servo].write(180);
-    currentPosition[servo] = OFF;
-    cout << "moveOff servo=" << servo << endl;
+class DMXSwitch {
+public:
+  DMXSwitch(const int slaveChannels) : dmx_(slaveChannels) {
+    lastValues_ = new SwitchPosition[slaveChannels]();
+      dmx_.enable();
   }
-}
 
-void moveOn(int servo) {
-  if(servo < DMX_SLAVE_CHANNELS) {
-    servoList[servo].write(0);
-    currentPosition[servo] = ON;
-    cout << "moveOn servo=" << servo << endl;
+  ~DMXSwitch() {
+    delete [] lastValues_;
   }
-}
 
-void moveNeutral(int servo) {
-  if(servo < DMX_SLAVE_CHANNELS) {
-    servoList[servo].write(90);
-    cout << "moveNeutral servo=" << servo << endl;
+  SwitchPosition getSwitchFromDmx(int offset) {
+    const int dmxValue = dmx_.getChannelValue(offset);
+    if(dmxValue > 2 && dmxValue  < 127) {
+      lastValues_[offset] = OFF;
+      return OFF;
+    }
+    if(dmxValue >= 127) {
+      lastValues_[offset] = ON;
+      return ON;
+    }
+    return lastValues_[offset];
   }
-}
+
+  void setStartAddress(const int start) {
+    // Set start address
+    // You can change this address at any time during the program
+    dmx_.setStartAddress(start);
+  }
+  DMX_Slave& getDmx() { return dmx_; }
+private:
+  DMX_Slave dmx_;
+  SwitchPosition* lastValues_;
+};
+
+class ServoSwitch {
+public:
+  ServoSwitch(const int id, const int pin)
+    : id_(id)
+    , switchPosition_(NEUTRAL)
+    , availableToMoveAgain_(0)
+    , pendingMoveToNeutral_(0) {
+      servo_.attach(pin); // Attach servos to pins starting at 3
+      moveNeutral();  // Start servo at neutral position
+  }
+
+  void doCycle(const SwitchPosition proposedPosition, const unsigned long currentMillis) {
+    handlePendingNeutral(currentMillis);
+    maybeDmxMoveServo(proposedPosition, currentMillis);
+  }
+
+private:
+  void handlePendingNeutral(const unsigned long currentMillis) {
+    if(availableToMoveAgain_ != 0 && availableToMoveAgain_ <  currentMillis) {
+      // delay time has elapsed, we can move again
+      cout << "Clearing pending move servo=" << id_ << endl;
+      availableToMoveAgain_ = 0;
+      pendingMoveToNeutral_ = 0;
+    } else if(pendingMoveToNeutral_!= 0 && pendingMoveToNeutral_ < currentMillis && availableToMoveAgain_==0) {
+        // move neutral and setup a timer to make sure servo completes neutral
+        availableToMoveAgain_ = currentMillis + MOVE_DELAY;
+        moveNeutral();
+        cout << "Moving to neutral after position change servo=" << id_ << endl;
+    } else {
+      cout <<"no pending servo=" << id_ << endl;
+    }
+  }
+
+  void maybeDmxMoveServo(const SwitchPosition proposedPosition, const unsigned long currentMillis) {
+    if(canDmxMoveServo(proposedPosition)) {
+      pendingMoveToNeutral_ = currentMillis + MOVE_DELAY;
+      cout <<"pendingMoveToNeutral[" << id_ << "]" << "=" << pendingMoveToNeutral_ << endl;
+      switch(proposedPosition) {
+        case ON:
+          moveOn();
+          break;
+        case OFF:
+          moveOff();
+          break;
+        case NEUTRAL:
+          // Don't want dmx to cause a movement
+          break;
+      }
+    }
+  }
+
+  bool canDmxMoveServo(const SwitchPosition proposedPosition) {
+    // DMX cannot move to neutral
+    if(proposedPosition == NEUTRAL) {
+      cout << "cannot move to neutral from dmx servo=" <<id_ << endl;
+      return false;
+    }
+    // Move if this is a new position and no movements are in progress.
+    if(proposedPosition != switchPosition_) {
+      if(pendingMoveToNeutral_==0) {
+        return true;
+      } else {
+        cout << "cannot move during pending neutral servo=" << id_ << endl;
+        return false;
+      }
+    } else {
+      cout << "no need to move servo=" << id_ << endl;
+      return false;
+    }
+  }
+
+  void moveNeutral() {
+    servo_.write(90);
+    cout << "moveNeutral servo=" << id_ << endl;
+  }
+
+  void moveOff() {
+    servo_.write(180);
+    switchPosition_ = OFF;
+    cout << "moveOff servo=" << id_ << endl;
+  }
+
+  void moveOn() {
+    servo_.write(0);
+    switchPosition_ = ON;
+    cout << "moveOn servo=" << id_ << endl;
+  }
+
+  int id_;
+  Servo servo_;
+  SwitchPosition switchPosition_;
+  unsigned long availableToMoveAgain_;
+  unsigned long pendingMoveToNeutral_;
+};
+
+
+DMXSwitch* gDmxSwitch(nullptr);
+std::vector<ServoSwitch*> gServoSwitches;;
 
 void setup() {
-  for(int i=0 ; i < DMX_SLAVE_CHANNELS ; ++i) {
-    servoList[i].attach(3+i); // Attach servos to pins starting at 3
-    moveNeutral(i);  // Start servo at neutral position
-    currentPosition[i] = NEUTRAL;
-  }
-
-  // Enable DMX slave interface and start recording
-  // DMX data
-  dmx_slave.enable();
-
+  gDmxSwitch = new DMXSwitch(DMX_SLAVE_CHANNELS);
   // Set start address
   // You can change this address at any time during the program
-  dmx_slave.setStartAddress (501);
-}
+  gDmxSwitch->setStartAddress(501);
 
-
-SwitchPosition getSwitchFromDmx(int offset) {
-  const int dmxValue = dmx_slave.getChannelValue(offset);
-  if(dmxValue > 2 && dmxValue  < 127) {
-    return OFF;
-  }
-  if(dmxValue >= 127) {
-    return ON;
-  }
-  return NEUTRAL;
-}
-
-void handlePendingNeutral(int servo, unsigned long currentMillis) {
-  if(availableToMoveAgain[servo] != 0 && availableToMoveAgain[servo] <  currentMillis) {
-    // delay time has elapsed, we can move again
-    cout << "Clearing pending move servo=" << servo << endl;
-    availableToMoveAgain[servo] = 0;
-    pendingMoveToNeutral[servo] = 0;
-  } else if(pendingMoveToNeutral[servo]!= 0 && pendingMoveToNeutral[servo] < currentMillis && availableToMoveAgain[servo]==0) {
-      // move neutral and setup a timer to make sure servo completes neutral
-      availableToMoveAgain[servo] = currentMillis + MOVE_DELAY;
-      moveNeutral(servo);
-      cout << "Moving to neutral after position change servo=" << servo << endl;
-  } else {
-    cout <<"no pending servo=" << servo << endl;
-  }
-}
-
-bool canDmxMoveServo(int servo, SwitchPosition proposedPosition) {
-  // DMX cannot move to neutral
-  if(proposedPosition == NEUTRAL) {
-    return false;
-  }
-  // Move if this is a new position and no movements are in progress.
-  return proposedPosition != currentPosition[servo] && pendingMoveToNeutral[servo]==0;
-}
-
-void maybeDmxMoveServo(int servo, SwitchPosition proposedPosition, unsigned long currentMillis) {
-  if(canDmxMoveServo(servo, proposedPosition)) {
-    pendingMoveToNeutral[servo] = currentMillis + MOVE_DELAY;
-    cout <<"pendingMoveToNeutral[" << servo << "]" << "=" << pendingMoveToNeutral[servo] << endl;
-    switch(proposedPosition) {
-      case ON:
-        moveOn(servo);
-        break;
-      case OFF:
-        moveOff(servo);
-        break;
-      case NEUTRAL:
-        // Don't want dmx to cause a movement
-        break;
-    }
+  for(int i=0 ; i < DMX_SLAVE_CHANNELS ; ++i) {
+    gServoSwitches.push_back(new ServoSwitch(i, i+3));
   }
 }
 
 void loop() {
   unsigned long currentMillis = millis();
   cout << "currentMillis " << currentMillis << endl;
-  for (int servo = 0; servo < DMX_SLAVE_CHANNELS; ++servo){
-    handlePendingNeutral(servo, currentMillis);
-    SwitchPosition proposedPosition = getSwitchFromDmx(servo);
-    maybeDmxMoveServo(servo, proposedPosition, currentMillis);
-  }
+  std::for_each(gServoSwitches.begin(), gServoSwitches.end(),
+    [*](ServoSwitch* servoSwitch) {
+      servoSwitch->doCycle(gDmxSwitch->getSwitchFromDmx(i), currentMillis);
+    });
 }
 
 int main() {
@@ -130,8 +176,8 @@ int main() {
   loop();
   std::this_thread::sleep_for(std::chrono::milliseconds(3000));
   cout << "-----" << endl;
-  dmx_slave.setChannelValue(0, 50);
-  dmx_slave.setChannelValue(1, 128);
+  gDmxSwitch->getDmx().setChannelValue(0, 50);
+  gDmxSwitch->getDmx().setChannelValue(1, 128);
 
   loop();
   std::this_thread::sleep_for(std::chrono::milliseconds(3000));
@@ -149,8 +195,14 @@ int main() {
   std::this_thread::sleep_for(std::chrono::milliseconds(3000));
   cout << "-----" << endl;
 
-  dmx_slave.setChannelValue(0, 200);
-  dmx_slave.setChannelValue(1, 5);
+  gDmxSwitch->getDmx().setChannelValue(0, 200);
+  gDmxSwitch->getDmx().setChannelValue(1, 5);
+
+  loop();
+  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+  cout << "-----" << endl;
+
+  gDmxSwitch->getDmx().setChannelValue(1, 200);
 
   loop();
   std::this_thread::sleep_for(std::chrono::milliseconds(3000));
@@ -164,8 +216,4 @@ int main() {
   std::this_thread::sleep_for(std::chrono::milliseconds(3000));
   cout << "-----" << endl;
 
-  loop();
-  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-  cout << "-----" << endl;
-  
 }
